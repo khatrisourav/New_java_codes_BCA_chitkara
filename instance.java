e1e8f075672519df153832f6d9e864eb820ea5cc
khatrisourav <sourabhbatra403@gmail.com>
2025-06-13 12:01:24 +0530

    new instance_of_operator

---
+++ instance.java
@@ -0,0 +1,16 @@
+
+
+
+public class instance {
+int x ;
+
+public static void main (String [] args ){
+
+instance yogi = new instance();
+if (yogi instanceof instance){
+System.out.println ("Yes this is object of instance class");
+
+}
+}
+
+}
